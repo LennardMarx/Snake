a4effa6cce67a49364341d831b77ab7aebcf76c2
LennardMarx <l.marx@gmx.de>
2022-10-08 17:53:56 +0200

    quasi finished

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 g++ src/*.cpp -std=c++17 -I/usr/include/SDL2/ -I. -Iinclude/ -lSDL2main -lSDL2 -lSDL2_ttf -o snake
 
 #g++ -g -std=c++17 ./src/*.cpp -I./include -o ./bin/snake -lSDL2 -lSDL2_ttf -ldl
